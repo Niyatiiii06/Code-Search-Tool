1dd62ea77c5401e93c46ac1752ddb2379ef443d5
Niyati Singh <correct-sniyati0827@gmail.com>
2026-08-11 16:23:24 +0530

    Add AST-based function definition search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import ast
 IGNORED_DIRS = {
     ".git",
     ".venv",
@@ -9,47 +10,40 @@ IGNORED_DIRS = {
 
 def search_code(project_path, function_name):
     results = []
+
     for root, dirs, files in os.walk(project_path):
+
         dirs[:] = [
             directory
             for directory in dirs
             if directory not in IGNORED_DIRS
         ]
+
         for file in files:
+
             if file.endswith(".py"):
+
                 filepath = os.path.join(root, file)
+
                 with open(filepath, "r", encoding="utf-8") as f:
-                    lines = f.readlines()
-                for line_no, line in enumerate(lines, start=1):
+                    code = f.read()
 
-                    if f"def {function_name}(" in line:
+                tree = ast.parse(code)
 
-                        results.append({
-                            "type": "DEFINITION",
-                            "file": filepath,
-                            "line": line_no,
-                            "code": line.strip()
-                        })
+                for node in ast.walk(tree):
 
-                    elif f"import {function_name}" in line:
+                    if isinstance(node, ast.FunctionDef):
 
-                        results.append({
-                            "type": "IMPORT",
-                            "file": filepath,
-                            "line": line_no,
-                            "code": line.strip()
-                        })
+                        if node.name == function_name:
 
-                    elif f"{function_name}(" in line:
+                            results.append({
+                                "type": "DEFINITION",
+                                "file": filepath,
+                                "line": node.lineno,
+                                "code": f"def {node.name}"
+                            })
 
-                        results.append({
-                            "type": "CALL",
-                            "file": filepath,
-                            "line": line_no,
-                            "code": line.strip()
-                        })
     return results
-
 
 project_path = input("Enter project path: ")
 function_name = input("Enter function name: ")
