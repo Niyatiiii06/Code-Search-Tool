b6deac07b9068d045b896d124701c434e3a1a47a
Niyati Singh <correct-sniyati0827@gmail.com>
2026-08-11 16:04:54 +0530

    Return structured code search results

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
 import os
 
+
+def search_code(project_path, function_name):
+    results = []
+    for root, dirs, files in os.walk(project_path):
+        for file in files:
+            if file.endswith(".py"):
+                filepath = os.path.join(root, file)
+                with open(filepath, "r", encoding="utf-8") as f:
+                    lines = f.readlines()
+                for line_no, line in enumerate(lines, start=1):
+
+                    if f"def {function_name}" in line:
+                        results.append(
+                            f"DEFINITION → {filepath}:{line_no}"
+                        )
+
+                    elif f"import {function_name}" in line:
+                        results.append(
+                            f"IMPORT → {filepath}:{line_no}"
+                        )
+
+                    elif f"{function_name}(" in line:
+                        results.append(
+                            f"CALL → {filepath}:{line_no}"
+                        )
+    return results
+
+
 project_path = "sample_project"
 function_name = input("Enter function name: ")
+results = search_code(project_path, function_name)
 
-for root, dirs, files in os.walk(project_path):
-    for file in files:
-        if file.endswith(".py"):
-            filepath=os.path.join(root, file)
-            with open(filepath, 'r', encoding= 'utf-8') as f:
-                lines= f.readlines()
-            for line_no, line in enumerate(lines,start=1):
-                if f"def {function_name}(" in line:
-                    print(
-                        f"DEFINITION → {filepath}:{line_no}"
-                    )
-
-                elif f"import {function_name}" in line:
-                    print(
-                        f"IMPORT → {filepath}:{line_no}"
-                    )
-
-                elif f"{function_name}(" in line:
-                    print(
-                        f"CALL → {filepath}:{line_no}"
-                    )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for result in results:
+    print(result)
